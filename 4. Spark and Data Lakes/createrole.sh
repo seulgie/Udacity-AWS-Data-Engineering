ebbed4e9aada5200b2728c748a069055f7fcb8ed
Seulgie Han <gkstmfrl917@naver.com>
2024-11-03 09:59:09 +0100

    Create createrole.sh

---
+++ 4. Spark and Data Lakes/createrole.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ aws iam create-role --role-name my-glue-service-role --assume-role-policy-document '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "glue.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}'
