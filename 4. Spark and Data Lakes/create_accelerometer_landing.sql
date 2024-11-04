5feac2d1bfa5ece90dbdbc5c9cd0cd235a222b26
Seulgie Han <gkstmfrl917@naver.com>
2024-11-04 21:16:19 +0100

    Update and rename accelerometer_landing.sql to create_accelerometer_landing.sql

---
+++ 4. Spark and Data Lakes/create_accelerometer_landing.sql
@@ -0,0 +1,12 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `seulgie`.`accelerometer_landing` (
+  `user` string,
+  `timeStamp` bigint,
+  `x` float,
+  `y` float,
+  `z` float
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES ('serialization.format' = '1')
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://seulgie-lake-house/accelerometer/landing/'
+TBLPROPERTIES ('classification' = 'json');
